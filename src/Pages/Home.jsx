408a1d44148397dfd6ec576c5eec238d3a179b4f
Nayan Sarkar <10292012+nayan-sarkar@users.noreply.github.com>
2023-12-12 18:58:30 -0800

    updates:

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home(){
             <HeroBanner/>
             <CustomList query= {"trending"} listName = {"Trending"}/>
             <MovieList name={"Now Playing"} genre_page={"now_playing,2"}/>
-            <MovieList name={"Upcoming"} genre_page={"upcoming,4"}/>
+            <MovieList name={"Upcoming"} genre_page={"upcoming,3"}/>
             <MovieList name={"Popular"} genre_page={"popular,3"}/>
             <MovieList name={"Top Rated"} genre_page={"top_rated,2"}/>
         </>
